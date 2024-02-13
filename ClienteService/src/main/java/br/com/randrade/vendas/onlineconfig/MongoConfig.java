172a5d428d58739e463287ade224f2fbb4ce77a6
Rafael Andrade <rafaandrade2810@gmail.com>
2024-02-13 04:19:31 -0300

    Configurado ClienteService para implementação correta no MongoDB

---
+++ ClienteService/src/main/java/br/com/randrade/vendas/onlineconfig/MongoConfig.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package br.com.randrade.vendas.onlineconfig;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+/**
+ * @author rafael.andrade
+ *
+ */
+@Configuration
+@EnableMongoRepositories(basePackages = "br.com.randrade.vendas.online.repository")
+public class MongoConfig {
+
+}
---
+++ ClienteService/src/main/java/br/com/randrade/vendas/onlineconfig/ValidatorConfig.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package br.com.randrade.vendas.onlineconfig;
+
+import jakarta.validation.Validator;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+/**
+ * @author rafael.andrade
+ *
+ */
+@Configuration
+public class ValidatorConfig {
+	
+//	@Autowired
+//    ResourceBundleMessageSource messageSource;
+
+	@Bean
+    public Validator validatorFactory () {
+		LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+//	    bean.setValidationMessageSource(messageSource);
+	    return bean;
+    }
+}
