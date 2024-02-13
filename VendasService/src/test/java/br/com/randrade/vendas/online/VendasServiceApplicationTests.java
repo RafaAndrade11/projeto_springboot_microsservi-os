dc36d442d2912347648bb885919322c0d789c9fb
Rafael Andrade <rafaandrade2810@gmail.com>
2024-02-12 21:11:41 -0300

    First commit

---
+++ VendasService/src/test/java/br/com/randrade/vendas/online/VendasServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.randrade.vendas.online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
